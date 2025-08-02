5ded0bdba68675a01e83c2299a6eab0b067a128b
Jacob Rodriguez <jacobrdz47@gmail.com>
2025-08-02 11:49:53 -0500

    Added pyright to lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -118,6 +118,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         tsserver = {},
+        pyright = {},
         html = {},
         jsonls = {},
         lua_ls = {
@@ -279,17 +280,12 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      -- ADD LSP servers HERE
       local servers = {
         -- tsserver = {},
         html = {},
         jsonls = {},
+        pyright = {},
         tailwindcss = {},
         cssls = {},
         clangd = {
